d9bebc897197031deac43afc6cc3185cfe54f70a
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-05-27 13:25:43 -0300

    merge queries for brand, name and model attributes into a search one in product repository

--- backend/products_mService/src/main/java/com/nahuelgg/inventory_app/products/repositories/ProductRepository.java
+++ backend/products_mService/src/main/java/com/nahuelgg/inventory_app/products/repositories/ProductRepository.java
@@ -14,7 +14,16 @@ public interface ProductRepository extends JpaRepository<ProductEntity, UUID>{
   @Query("select p from ProductEntity p where p.categories.name = ?1")
   List<ProductEntity> findByCategoryName(String category);
 
-  List<ProductEntity> findByBrand(String brand);
-
-  List<ProductEntity> findByName(String name);
+  /*
+    Por cada producto que revise se fijará cada una de las condiciones.
+    Cada condición devolverá true si coincide con el parámetro o el valor de este es nulo,
+    lo que por ejemplo si no se envía ningún parámetro la query devolverá todos los productos.
+    ya que todas las condiciones se cumplieron
+  */
+  @Query("select p from ProductEntity p where" + 
+    "(p.brand = ?1 or ?1 is null) and" +
+    "(p.name = ?2 or ?2is null) and" +
+    "(p.model like %?3% or ?3 is null)"
+  )
+  List<ProductEntity> findByBrandNameAndModel(String brand, String name, String model);
 }
