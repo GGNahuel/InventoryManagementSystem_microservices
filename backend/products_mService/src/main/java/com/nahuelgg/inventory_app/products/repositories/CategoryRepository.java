efc11cfbbf207e396bd537c25a7f6e52e2c73576
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-05-27 13:25:56 -0300

    add findByName in category repository

--- backend/products_mService/src/main/java/com/nahuelgg/inventory_app/products/repositories/CategoryRepository.java
+++ backend/products_mService/src/main/java/com/nahuelgg/inventory_app/products/repositories/CategoryRepository.java
@@ -1,14 +1,14 @@
 package com.nahuelgg.inventory_app.products.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nahuelgg.inventory_app.products.Entities.CategoryEntity;
-import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, UUID> {
-  List<CategoryEntity> findByName(String name);
+  Optional<CategoryEntity> findByName(String name);
 }
