04349d265adcaaf86c3bbe91b30d4b56dd16e893
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-05-28 23:23:06 -0300

    change return type in account service interface

--- backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/services/AccountService.java
+++ backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/services/AccountService.java
@@ -6,12 +6,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.nahuelgg.inventory_app.users.dtos.AccountDTO;
 import com.nahuelgg.inventory_app.users.dtos.UserDTO;
-import com.nahuelgg.inventory_app.users.entities.UserEntity;
 
 public interface AccountService extends UserDetailsService {
   AccountDTO getById(UUID id);
   AccountDTO create(String username, String password, String passwordRepeated, String adminPassword, String adminPasswordRepeated);
-  UserEntity addUser(UserDTO user, UUID accountId);
+  UserDTO addUser(UserDTO user, UUID accountId);
   void assignInventory(UUID accountId, String inventoryId);
   void removeInventoryAssigned(UUID accountId, String inventoryId);
   // este método también tendría que eliminar todos los inventarios asociados y a su vez los productos asociados a estos,
