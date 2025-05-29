d333c0cb032db679480bd51712918ae19f5e56f5
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-05-28 23:22:34 -0300

    correct type of return for query in inventoryRef repository

--- backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/repositories/InventoryRefRepository.java
+++ backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/repositories/InventoryRefRepository.java
@@ -1,14 +1,14 @@
 package com.nahuelgg.inventory_app.users.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nahuelgg.inventory_app.users.entities.InventoryRefEntity;
-import java.util.List;
 
 @Repository
 public interface InventoryRefRepository extends JpaRepository<InventoryRefEntity, UUID>{
-  List<InventoryRefEntity> findByInventoryIdReference(String inventoryIdReference);
+  Optional<InventoryRefEntity> findByInventoryIdReference(String inventoryIdReference);
 }
