b83ee0e4b894096182abb6a9b781ee7315c6fa4d
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-05-31 20:45:18 -0300

    add query to find inventories by name and acc id

--- backend/inventory_mService/src/main/java/com/nahuelgg/inventory_app/inventories/repositories/InventoryRepository.java
+++ backend/inventory_mService/src/main/java/com/nahuelgg/inventory_app/inventories/repositories/InventoryRepository.java
@@ -3,6 +3,7 @@ package com.nahuelgg.inventory_app.inventories.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nahuelgg.inventory_app.inventories.entities.InventoryEntity;
@@ -10,5 +11,8 @@ import java.util.List;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<InventoryEntity, UUID> {
+  @Query("select i from InventoryEntity i where i.name = ?1 and i.accountId = ?2")
+  List<InventoryEntity> findByNameAndAccountId(String name, UUID accountId);
+
   List<InventoryEntity> findByAccountId(UUID accountId);
 }
