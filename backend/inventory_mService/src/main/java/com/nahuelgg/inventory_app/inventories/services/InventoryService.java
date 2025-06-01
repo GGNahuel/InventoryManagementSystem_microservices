402d50f3197d5c2b8c502f85d95b8168992e5e63
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-06-01 09:59:40 -0300

    add restTemplate bean, change some methods args and return types in inv service interface

--- backend/inventory_mService/src/main/java/com/nahuelgg/inventory_app/inventories/services/InventoryService.java
+++ backend/inventory_mService/src/main/java/com/nahuelgg/inventory_app/inventories/services/InventoryService.java
@@ -10,16 +10,16 @@ import com.nahuelgg.inventory_app.inventories.dtos.ProductToCopyDTO;
 import com.nahuelgg.inventory_app.inventories.dtos.UserFromUsersMSDTO;
 
 public interface InventoryService {
-  List<InventoryDTO> getAll();
   InventoryDTO getById(UUID id);
   List<InventoryDTO> getByAccount(UUID accountID);
   InventoryDTO getByNameAndAccount(String name, UUID accountId);
-  List<InventoryDTO> searchProductsInInventories(String name, String brand, String model, List<String> categories, UUID accountId);
+  List<InventoryDTO> searchProductsInInventories(
+    String name, String brand, String model, List<String> categories, UUID accountId);
   InventoryDTO create(String name, UUID accountId);
-  InventoryDTO edit(UUID id, String name);
+  boolean edit(UUID id, String name);
 
   boolean addUser(UserFromUsersMSDTO user, UUID invId);
   ProductInInvDTO addProduct(ProductInputDTO product, UUID invID);
-  InventoryDTO copyProducts(List<ProductToCopyDTO> products, UUID idFrom, UUID idTo);
-  ProductInInvDTO editStockOfProduct(Integer newStock, UUID productRefId, UUID invId);
+  boolean copyProducts(List<ProductToCopyDTO> products, UUID idTo);
+  boolean editStockOfProduct(int relativeNewStock, UUID productRefId, UUID invId);
 }
