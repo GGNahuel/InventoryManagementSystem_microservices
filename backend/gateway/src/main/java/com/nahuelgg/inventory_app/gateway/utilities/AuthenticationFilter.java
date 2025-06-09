ff209eaad78f9f5035c9f25ffc490c274d5acb8c
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-06-09 00:40:49 -0300

    add authentication filter to gateway

---
+++ backend/gateway/src/main/java/com/nahuelgg/inventory_app/gateway/utilities/AuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.nahuelgg.inventory_app.gateway.utilities;
+
+import java.util.List;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter> {
+  private final JwtUtils jwtUtils;
+
+  public AuthenticationFilter(JwtUtils jwtUtils) {
+    this.jwtUtils = jwtUtils;
+  }
+
+  private boolean checkIfEndpointIsOpen(ServerHttpRequest request) {
+    List<String> openEndpoints = List.of(
+      "/account/register",
+      "/logincheck"
+    );
+
+    return openEndpoints.stream().noneMatch(uri -> request.getURI().getPath().contains(uri));
+  }
+
+  @Override
+  public GatewayFilter apply(AuthenticationFilter config) {
+    return ((exchange, chain) -> {
+      if (checkIfEndpointIsOpen(exchange.getRequest())) {
+        if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+          throw new RuntimeException("Missing authorization header");
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+          authHeader = authHeader.substring(7);
+        }
+        try {
+          jwtUtils.validateToken(authHeader);
+        } catch (Exception e) {
+          System.out.println("invalid access...!");
+          throw new RuntimeException("unauthorized access to application");
+        }
+      }
+      return chain.filter(exchange);
+    });
+  }
+}
---
+++ backend/gateway/src/main/java/com/nahuelgg/inventory_app/gateway/utilities/JwtUtils.java
@@ -0,0 +1,25 @@
+package com.nahuelgg.inventory_app.gateway.utilities;
+
+import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtils {
+  @Value("${JWT_KEY}")
+  private String secretKey;
+
+  public void validateToken(final String token) {
+    Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+  }
+
+  private Key getSignKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+}
