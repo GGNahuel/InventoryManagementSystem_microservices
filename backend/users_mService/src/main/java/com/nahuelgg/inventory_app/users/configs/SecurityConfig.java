5e473ddf973bb5c855c8bca924197d7bae4f3b02
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-06-11 22:40:18 -0300

    implement changes in authentication in securityConfig

--- backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/configs/SecurityConfig.java
+++ backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/configs/SecurityConfig.java
@@ -2,16 +2,23 @@ package com.nahuelgg.inventory_app.users.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+  private final AuthenticationProvider authenticationProvider;
+
   @Bean
   SecurityFilterChain setFilterChainProps(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
@@ -22,9 +29,16 @@ public class SecurityConfig {
       .sessionManagement(session ->
         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       )
+      .authenticationProvider(authenticationProvider)
+      .addFilterBefore(jwtRequestFilterConfig(), UsernamePasswordAuthenticationFilter.class)
       .csrf(csrf -> csrf.disable());
 
     return httpSecurity.build();
+  }
+
+  @Bean
+  JwtRequestFilterConfig jwtRequestFilterConfig() {
+    return new JwtRequestFilterConfig();
   }
 }
 
