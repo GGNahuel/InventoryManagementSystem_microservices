bf41e489b768149922e1ce7bdaeff2f56c960796
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-06-23 22:48:47 -0300

    change how jwtFilter set the context

--- backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/configs/JwtRequestFilterConfig.java
+++ backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/configs/JwtRequestFilterConfig.java
@@ -3,15 +3,18 @@ package com.nahuelgg.inventory_app.users.configs;
 import java.io.IOException;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.nahuelgg.inventory_app.users.dtos.JwtClaimsDTO;
 import com.nahuelgg.inventory_app.users.services.JwtService;
+import com.nahuelgg.inventory_app.users.utilities.ContextAuthenticationPrincipal;
+import com.nahuelgg.inventory_app.users.utilities.ContextAuthenticationPrincipal.AccountSigned;
+import com.nahuelgg.inventory_app.users.utilities.ContextAuthenticationPrincipal.PermsForInv;
+import com.nahuelgg.inventory_app.users.utilities.ContextAuthenticationPrincipal.UserSigned;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -23,33 +26,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtRequestFilterConfig extends OncePerRequestFilter {
   private final JwtService jwtService;
-  private final UserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
   throws ServletException, IOException {
     String authHeader = request.getHeader("Authorization");
-    String token;
-    String accountUsername;
-
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
       filterChain.doFilter(request, response);
       return;
     }
 
+    String token = authHeader.substring(7);
+
     try {
-      token = authHeader.substring(7);
+      String accountUsername = jwtService.getClaim(token, claim -> claim.getSubject());
 
-      accountUsername = jwtService.getClaim(token, claim -> claim.getSubject());
-
-      if (accountUsername == null || SecurityContextHolder.getContext().getAuthentication() != null) {
+      if (accountUsername == null) {
         filterChain.doFilter(request, response);
         return;
       }
 
-      UserDetails userDetails = userDetailsService.loadUserByUsername(accountUsername);
-
-      boolean isTokenValid = jwtService.isTokenValid(token, userDetails.getUsername());
+      JwtClaimsDTO tokenClaims = jwtService.mapTokenClaims(token);
+      boolean isTokenValid = jwtService.isTokenValid(token, accountUsername);
       boolean isTokenExpired = jwtService.isTokenExpired(token);
       boolean canBeRenewed = jwtService.canTokenBeRenewed(token);
 
@@ -60,22 +58,35 @@ public class JwtRequestFilterConfig extends OncePerRequestFilter {
       }
 
       if (isTokenExpired) {
-        JwtClaimsDTO oldTokenClaims = jwtService.mapTokenClaims(token);
-        String newToken = jwtService.renewToken(token, oldTokenClaims);
+        String newToken = jwtService.renewToken(token, tokenClaims);
         response.setHeader("Authorization", "Bearer " + newToken);
       }
 
-      UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-        userDetails,
+      Authentication existingAuth = SecurityContextHolder.getContext().getAuthentication();
+      if (existingAuth != null && existingAuth.getName().equals(accountUsername)) {
+        filterChain.doFilter(request, response);
+        return;
+      }
+
+      ContextAuthenticationPrincipal newAuthData = ContextAuthenticationPrincipal.builder()
+        .account(new AccountSigned(accountUsername, ""))
+        .user(new UserSigned(
+          tokenClaims.getUserName(), tokenClaims.getUserRole(), tokenClaims.isAdmin(), 
+          tokenClaims.getUserPerms().stream().map(
+            permDto -> new PermsForInv(permDto.getIdOfInventoryReferenced(), permDto.getPermissions())
+          ).toList()
+        ))
+      .build();
+
+      UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(
+        newAuthData,
         null,
-        userDetails.getAuthorities()
+        null
       );
 
-      authToken.setDetails(
-          new WebAuthenticationDetailsSource().buildDetails(request));
+      newAuth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-      SecurityContextHolder.getContext().setAuthentication(authToken);
-
+      SecurityContextHolder.getContext().setAuthentication(newAuth);
     } catch (Exception e) {
       // TODO: 
       System.out.println(e.getMessage());
