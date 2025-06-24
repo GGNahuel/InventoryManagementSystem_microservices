f143062c792427831821de5862555d1bdf7725d8
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-06-23 22:49:05 -0300

    test account controller  when endpoint success

--- backend/users_mService/src/test/java/com/nahuelgg/inventory_app/users/controller/Test_AccountController.java
+++ backend/users_mService/src/test/java/com/nahuelgg/inventory_app/users/controller/Test_AccountController.java
@@ -1,46 +1,52 @@
 package com.nahuelgg.inventory_app.users.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.times;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nahuelgg.inventory_app.users.configs.SecurityConfig;
-import com.nahuelgg.inventory_app.users.controllers.AccountController;
 import com.nahuelgg.inventory_app.users.dtos.AccountDTO;
+import com.nahuelgg.inventory_app.users.dtos.JwtClaimsDTO;
+import com.nahuelgg.inventory_app.users.dtos.PermissionsForInventoryDTO;
 import com.nahuelgg.inventory_app.users.dtos.ResponseDTO;
 import com.nahuelgg.inventory_app.users.dtos.UserDTO;
-import com.nahuelgg.inventory_app.users.entities.UserEntity;
+import com.nahuelgg.inventory_app.users.repositories.AccountRepository;
 import com.nahuelgg.inventory_app.users.services.AccountService;
-import com.nahuelgg.inventory_app.users.services.AuthorizationService;
-import com.nahuelgg.inventory_app.users.utilities.Constants;
+import com.nahuelgg.inventory_app.users.services.JwtService;
 
-@WebMvcTest(AccountController.class)
-@AutoConfigureMockMvc(addFilters = false)
-@Import(SecurityConfig.class)
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Test_AccountController {
-  @Autowired MockMvc mockMvc;
+  @Autowired TestRestTemplate restTemplate;
   @Autowired ObjectMapper objectMapper;
+  @Mock AccountRepository repository;
 
   @MockitoBean AccountService service;
-  @MockitoBean(name = "authorizationService") AuthorizationService authorizationService;
+  @MockitoBean JwtService jwtService;
 
   AccountDTO acc = AccountDTO.builder()
     .id(UUID.randomUUID().toString())
@@ -48,136 +54,150 @@ public class Test_AccountController {
     .users(new ArrayList<>())
   .build();
 
-  private MockHttpSession emulateLoginUser(boolean isAdmin) {
-    when(authorizationService.checkUserIsAdmin()).thenReturn(isAdmin);
+  String token = "testToken";
 
-    MockHttpSession session = new MockHttpSession();
-    UserEntity userEntity = UserEntity.builder().isAdmin(isAdmin).build();
-    session.setAttribute(Constants.userSessionAttr, userEntity);
+  private void configJwtMock(String userName, String userRole, boolean isAdmin, List<PermissionsForInventoryDTO> userPerms) {
+    when(jwtService.getClaim(eq(token), any())).thenAnswer(inv -> {
+      Function<Claims, ?> claimGetter = inv.getArgument(1);
+      Claims claims = Jwts.claims();
+      claims.setSubject(acc.getUsername());
+      claims.put("accountId", acc.getId());
+      claims.put("userName", userName);
+      claims.put("userRole", userRole);
+      claims.put("isAdmin", isAdmin);
+      claims.put("userPerms", userPerms);
+      return claimGetter.apply(claims);
+    });
+    when(jwtService.isTokenExpired(token)).thenReturn(false);
+    when(jwtService.isTokenValid(token, acc.getUsername())).thenReturn(true);
+    when(jwtService.mapTokenClaims(token)).thenReturn(JwtClaimsDTO.builder()
+      .accountId(acc.getId())
+      .userName(userName)
+      .userRole(userRole)
+      .isAdmin(isAdmin)
+      .userPerms(userPerms != null ? userPerms : List.of())
+    .build());
+  }
 
-    return session;
+  private HttpHeaders generateHeaderWithToken() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setBearerAuth(token);
+
+    return headers;
   }
 
   @Test
-  void getById() throws Exception {
-    when(service.getById(UUID.fromString(acc.getId()))).thenReturn(acc);
+  void getAll_successIfAuthenticated() {
+    List<AccountDTO> expected = List.of(acc);
+    when(service.getAll()).thenReturn(expected);
+    configJwtMock(null, null, false, null);
 
-    ResponseDTO response = objectMapper.readValue(
-      mockMvc.perform(MockMvcRequestBuilders.get("/account/" + acc.getId())).andReturn().getResponse().getContentAsString(),
-      ResponseDTO.class
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account", HttpMethod.GET, new HttpEntity<>(generateHeaderWithToken()), ResponseDTO.class);
+    List<AccountDTO> actual = objectMapper.convertValue(
+      response.getBody().getData(),
+      new TypeReference<List<AccountDTO>>() {}
     );
-    AccountDTO actual = objectMapper.convertValue(response.getData(), AccountDTO.class);
 
-    assertEquals(200, response.getStatus());
+    assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+    assertIterableEquals(expected, actual);
+  }
+
+
+  @Test
+  void getById() {
+    when(service.getById(UUID.fromString(acc.getId()))).thenReturn(acc);
+    configJwtMock(null, null, false, null);
+
+    HttpEntity<String> httpEntity = new HttpEntity<>(generateHeaderWithToken());
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account/id/" + acc.getId(), HttpMethod.GET, httpEntity, ResponseDTO.class);
+    assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+    AccountDTO actual = objectMapper.convertValue(response.getBody().getData(), AccountDTO.class);
     assertEquals(acc, actual);
   }
 
   @Test
-  void create() throws Exception {
+  void create_successEvenWithoutAuthentication() {
     String username = "user";
     String password = "password";
     String adminPassword = "adminPassword";
     when(service.create(username, password, password, adminPassword, adminPassword)).thenReturn(acc);
 
-    String url = UriComponentsBuilder.fromUriString("/account")
+    String url = UriComponentsBuilder.fromUriString("/account/register")
       .queryParam("username", username)
       .queryParam("password", password)
       .queryParam("passwordRepeated", password)
       .queryParam("adminPassword", adminPassword)
       .queryParam("adminPasswordRepeated", adminPassword)
     .toUriString();
-    ResponseDTO response = objectMapper.readValue(
-      mockMvc.perform(MockMvcRequestBuilders.post(url)).andReturn().getResponse().getContentAsString(),
-      ResponseDTO.class
-    );
-    assertNull(response.getError());
-    assertEquals(201, response.getStatus());
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange(url, HttpMethod.POST, null, ResponseDTO.class);
+    assertEquals(HttpStatusCode.valueOf(201), response.getStatusCode());
     
-    AccountDTO actual = objectMapper.convertValue(response.getData(), AccountDTO.class);
+    AccountDTO actual = objectMapper.convertValue(response.getBody().getData(), AccountDTO.class);
     assertEquals(acc, actual);
   }
 
   @Test
-  void addUser() throws Exception {
+  void addUser() {
     UserDTO user = UserDTO.builder().name("user").build();
     when(service.addUser(user, UUID.fromString(acc.getId()), "123", "123")).thenReturn(user);
+    configJwtMock("admin", "admin", true, null);
     
-    String url = UriComponentsBuilder.fromUriString("/account/add_user")
+    String url = UriComponentsBuilder.fromUriString("/account/add-user")
       .queryParam("accountId", acc.getId())
       .queryParam("password", "123")
       .queryParam("passwordRepeated", "123")
     .toUriString();
-    String body = objectMapper.writeValueAsString(user);
 
-    MockHttpSession session = emulateLoginUser(true);
-    ResponseDTO response = objectMapper.readValue(
-      mockMvc.perform(MockMvcRequestBuilders.post(url)
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(body)
-        .session(session)
-      ).andReturn().getResponse().getContentAsString(),
-      ResponseDTO.class
-    );
+    HttpEntity<UserDTO> httpEntity = new HttpEntity<UserDTO>(user, generateHeaderWithToken());
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, ResponseDTO.class);
+    assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
     
-    assertNull(response.getError());
-    assertEquals(200, response.getStatus());
-
-    UserDTO actual = objectMapper.convertValue(response.getData(), UserDTO.class);
+    UserDTO actual = objectMapper.convertValue(response.getBody().getData(), UserDTO.class);
     assertEquals(user, actual);
   }
 
   @Test
   void assignInventory() throws Exception {
     UUID invId = UUID.randomUUID();
-    MockHttpSession session = emulateLoginUser(true);
-    String url = UriComponentsBuilder.fromUriString("/account/add_inventory")
+    configJwtMock("admin", "admin", true, null);
+
+    String url = UriComponentsBuilder.fromUriString("/account/add-inventory")
       .queryParam("accountId", acc.getId())
       .queryParam("invId", invId.toString())
     .toUriString();
-    ResponseDTO response = objectMapper.readValue(
-      mockMvc.perform(MockMvcRequestBuilders.patch(url)
-        .session(session)
-      ).andReturn().getResponse().getContentAsString(),
-      ResponseDTO.class
-    );
 
-    assertNull(response.getError());
-    assertEquals(200, response.getStatus());
-    verify(service,times(1)).assignInventory(UUID.fromString(acc.getId()), invId);
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange(url, HttpMethod.PATCH, new HttpEntity<>(generateHeaderWithToken()), ResponseDTO.class);
+    assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+    verify(service).assignInventory(UUID.fromString(acc.getId()), invId);
   }
 
   @Test
   void removeInventoryAssigned() throws Exception {
     UUID invId = UUID.randomUUID();
-    MockHttpSession session = emulateLoginUser(true);
-    String url = UriComponentsBuilder.fromUriString("/account/remove_inventory")
+    configJwtMock("user", "role", true, null);
+
+    String url = UriComponentsBuilder.fromUriString("/account/remove-inventory")
       .queryParam("accountId", acc.getId())
       .queryParam("invId", invId.toString())
     .toUriString();
-    ResponseDTO response = objectMapper.readValue(
-      mockMvc.perform(MockMvcRequestBuilders.patch(url)
-        .session(session)
-      ).andReturn().getResponse().getContentAsString(),
-      ResponseDTO.class
-    );
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange(url, HttpMethod.PATCH, new HttpEntity<>(generateHeaderWithToken()), ResponseDTO.class);
+    assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
 
-    assertNull(response.getError());
-    assertEquals(200, response.getStatus());
-    verify(service,times(1)).removeInventoryAssigned(UUID.fromString(acc.getId()), invId);
+    verify(service).removeInventoryAssigned(UUID.fromString(acc.getId()), invId);
   }
 
   @Test
   void delete() throws Exception {
-    String url = UriComponentsBuilder.fromUriString("/account")
+    configJwtMock("user", "role", true, null);
+
+    String url = UriComponentsBuilder.fromUriString("/account/delete")
       .queryParam("id", acc.getId())
     .toUriString();
-    ResponseDTO response = objectMapper.readValue(
-      mockMvc.perform(MockMvcRequestBuilders.delete(url)).andReturn().getResponse().getContentAsString(),
-      ResponseDTO.class
-    );
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange(url, HttpMethod.DELETE, new HttpEntity<>(generateHeaderWithToken()), ResponseDTO.class);
 
-    assertNull(response.getError());
-    assertEquals(200, response.getStatus());
-    verify(service,times(1)).delete(UUID.fromString(acc.getId()));
+    assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+    verify(service).delete(UUID.fromString(acc.getId()));
   }
 }
