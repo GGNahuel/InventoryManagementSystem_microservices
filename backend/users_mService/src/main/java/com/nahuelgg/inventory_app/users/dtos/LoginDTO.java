324dc3b79a020c1ae7c1b3de63b3761ca33bb17e
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-07-25 00:16:28 -0300

    remove builder in login dto

--- backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/dtos/LoginDTO.java
+++ backend/users_mService/src/main/java/com/nahuelgg/inventory_app/users/dtos/LoginDTO.java
@@ -1,11 +1,10 @@
 package com.nahuelgg.inventory_app.users.dtos;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @Builder
+@Data
 @NoArgsConstructor @AllArgsConstructor
 public class LoginDTO {
   private String username;
