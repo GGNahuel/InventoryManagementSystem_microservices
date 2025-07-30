191b0de6e2cd53f009d775664c914a067188c3df
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-07-30 19:32:34 -0300

    fix exception handler in users service

--- backend/users_mService/src/test/java/com/nahuelgg/inventory_app/users/integration/ExceptionHandlerTest.java
+++ backend/users_mService/src/test/java/com/nahuelgg/inventory_app/users/integration/ExceptionHandlerTest.java
@@ -3,13 +3,14 @@ package com.nahuelgg.inventory_app.users.integration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,11 +34,9 @@ import com.nahuelgg.inventory_app.users.dtos.JwtClaimsDTO;
 import com.nahuelgg.inventory_app.users.dtos.ResponseDTO;
 import com.nahuelgg.inventory_app.users.exceptions.EmptyFieldException;
 import com.nahuelgg.inventory_app.users.exceptions.ResourceNotFoundException;
-import com.nahuelgg.inventory_app.users.services.JwtService;
 import com.nahuelgg.inventory_app.users.services.AccountService;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import com.nahuelgg.inventory_app.users.services.AuthorizationService;
+import com.nahuelgg.inventory_app.users.services.JwtService;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -47,24 +46,15 @@ public class ExceptionHandlerTest {
 
   @MockitoBean JwtService jwtService;
   @MockitoBean AccountService service;
+  @MockitoBean AuthorizationService authService;
 
   String token = "abcde";
   String accId = "12345";
   HttpEntity<String> entity;
 
   @BeforeEach
-  void configJwtMock() throws Exception {
-    when(jwtService.getClaim(eq(token), any())).thenAnswer(inv -> {
-      Function<Claims, ?> claimGetter = inv.getArgument(1);
-      Claims claims = Jwts.claims();
-      claims.setSubject("accUsername");
-      claims.put("accountId", accId);
-      claims.put("userName", "userName");
-      claims.put("userRole", "userRole");
-      claims.put("isAdmin", false);
-      claims.put("userPerms", List.of());
-      return claimGetter.apply(claims);
-    });
+  void configRequest() throws Exception {
+    when(jwtService.getClaim(eq(token), any())).thenReturn("accUsername");
     when(jwtService.isTokenExpired(token)).thenReturn(false);
     when(jwtService.isTokenValid(token, "accUsername")).thenReturn(true);
     when(jwtService.mapTokenClaims(token)).thenReturn(JwtClaimsDTO.builder()
@@ -75,6 +65,9 @@ public class ExceptionHandlerTest {
       .userPerms(List.of())
     .build());
 
+    when(authService.checkUserIsAdmin()).thenReturn(true);
+    when(authService.loggedAccountHasTheIdReferenced(anyString())).thenReturn(true);
+
     HttpHeaders headers = new HttpHeaders();
     headers.setBearerAuth(token);
 
@@ -83,15 +76,16 @@ public class ExceptionHandlerTest {
 
   @Test
   void emptyFieldEx() {
-    when(service.getAll()).thenThrow(EmptyFieldException.class);
-    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account", HttpMethod.GET, entity, ResponseDTO.class);
+    when(service.getById(any())).thenThrow(EmptyFieldException.class);
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account/id/" + UUID.randomUUID().toString(), HttpMethod.GET, entity, ResponseDTO.class);
+    System.out.println(response.toString());
     assertEquals(HttpStatusCode.valueOf(406), response.getStatusCode());
   }
 
   @Test
   void resourceNotFound() {
-    when(service.getAll()).thenThrow(ResourceNotFoundException.class);
-    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account", HttpMethod.GET, entity, ResponseDTO.class);
+    when(service.getById(any())).thenThrow(ResourceNotFoundException.class);
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account/id/" + UUID.randomUUID().toString(), HttpMethod.GET, entity, ResponseDTO.class);
     assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
   }
 
@@ -119,8 +113,8 @@ public class ExceptionHandlerTest {
 
   @Test
   void paramTypeMismatch() {
-    when(service.getAll()).thenThrow(MethodArgumentTypeMismatchException.class);
-    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account", HttpMethod.GET, entity, ResponseDTO.class);
+    when(service.getById(any())).thenThrow(MethodArgumentTypeMismatchException.class);
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account/id/" + UUID.randomUUID(), HttpMethod.GET, entity, ResponseDTO.class);
     assertEquals(HttpStatusCode.valueOf(400), response.getStatusCode());
   }
 
@@ -142,8 +136,8 @@ public class ExceptionHandlerTest {
   
   @Test
   void globalException() {
-    when(service.getAll()).thenThrow(RuntimeException.class);
-    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/product", HttpMethod.GET, entity, ResponseDTO.class);
+    when(service.getById(any())).thenThrow(RuntimeException.class);
+    ResponseEntity<ResponseDTO> response = restTemplate.exchange("/account/id/" + UUID.randomUUID(), HttpMethod.GET, entity, ResponseDTO.class);
     assertEquals(HttpStatusCode.valueOf(500), response.getStatusCode());
   }
 }
